a05e3424bc50e024c4b5fb79c4b6a9e5a797b841
jessica8melo <jessica8melo@gmail.com>
2024-08-20 01:57:21 -0300

    💩 tentativa *falha* de integrar interface com simulação

--- cliente.py
+++ cliente.py
@@ -1,19 +1,47 @@
 import socket
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 host = '127.0.0.1'
 port = 12345
-addr = (host, port)
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(addr)
 
-def enviar(msg):
-    bin_msg = msg.encode('utf8')
-    status = client.send(bin_msg)
-    
-    if status>0:
-        data = client.recv(1024)
-        print('Recebido', repr(data))
+class ClienteGTK(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Cliente")
 
-    client.close()
+        self.set_border_width(10)
+        self.set_default_size(300, 100)
 
-enviar(input())
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(self.box)
+
+        self.entry = Gtk.Entry()
+        self.box.pack_start(self.entry, True, True, 0)
+
+        self.button = Gtk.Button(label="Enviar")
+        self.button.connect("clicked", self.enviar)
+        self.box.pack_start(self.button, True, True, 0)
+
+        self.textview = Gtk.TextView()
+        self.textview.set_editable(False)
+        self.box.pack_start(self.textview, True, True, 0)
+
+    def enviar(self, widget):
+        msg = self.entry.get_text()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+            client.connect((host, port))
+            client.sendall(msg.encode('utf8'))
+            data = client.recv(1024)
+            self.exibir_resposta(data.decode('utf8'))
+
+    def exibir_resposta(self, resposta):
+        buffer = self.textview.get_buffer()
+        buffer.set_text(resposta)
+
+win = ClienteGTK()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+
+Gtk.main()
+
--- servidor.py
+++ servidor.py
@@ -1,4 +1,7 @@
 import socket
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 host = '127.0.0.1'
 port = 12345
@@ -6,14 +9,20 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
-def lidar_com_cliente(host, port):
+def lidar_com_cliente():
     conn, addr = server.accept()
     while True:
         data = conn.recv(1024)
         if not data:
             break
-        print(data.decode('utf-8'))
         conn.sendall(data)
     conn.close()
 
-lidar_com_cliente(host, port)
+def iniciar_servidor():
+    lidar_com_cliente()
+
+win = Gtk.Window(title="Servidor")
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+
+Gtk.main()
