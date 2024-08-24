2de8de296f51ee0fa67044483f02c9a0cfb7fcd0
Arthur Antero <arthurantero@hotmail.com>
2024-08-24 19:33:37 -0300

    fixing errors

--- servidor.py
+++ servidor.py
@@ -5,35 +5,50 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1', 12345))
 server.listen(5)
 
-clients = {}
-enderecos = {}
+clients = {}  # Armazena os clientes e seus nomes de usuário
 
 def lidar_cliente(client):
-    username = client.recv(1024).decode()
+    username = client.recv(1024).decode()  # Recebe o nome de usuário
+    clients[client] = username  # Adiciona o cliente à lista de clientes conectados
     client.send(bytes(f"Você está logado como {username}!", "utf8"))
     msg = f"{username} entrou no chat!"
-    enviar_msg(bytes(msg, "utf8"))
-    clients[client] = username
-    while True:
-        msg = client.recv(1024)
-        print(msg.decode().split(":"))
-        if msg != bytes("{quit}", "utf8"):
-            decoded_msg = msg.decode().split(":")
-            sender, message = decoded_msg
-            enviar_msg(bytes(f"{sender}: {message}", "utf8"))
+    enviar_msg(bytes(msg, "utf8"))  # Notifica todos os clientes que um novo usuário entrou no chat
 
-        else:
-            client.send(bytes("{quit}", "utf8"))
+    while True:
+        try:
+            msg = client.recv(2048)
+            if msg:
+                # Decodifica a mensagem recebida e separa o remetente e a mensagem
+                decoded_msg = msg.decode().split(": ", 1)  # Divide a string apenas uma vez
+                if len(decoded_msg) == 2:
+                    sender, message = decoded_msg
+                    enviar_msg(bytes(f"{sender}: {message}", "utf8"))
+                else:
+                    # Se o cliente enviou "{quit}", desconecta
+                    if msg.decode() == "{quit}":
+                        client.send(bytes("{quit}", "utf8"))
+                        client.close()
+                        del clients[client]
+                        enviar_msg(bytes(f"{username} saiu do chat.", "utf8"))
+                        break
+            else:
+                # Conexão fechada abruptamente
+                client.close()
+                del clients[client]
+                enviar_msg(bytes(f"{username} saiu do chat.", "utf8"))
+                break
+        except:
+            # Em caso de erro, desconecta o cliente
             client.close()
             del clients[client]
             enviar_msg(bytes(f"{username} saiu do chat.", "utf8"))
             break
 
-def enviar_msg(msg, prefix=""):
+def enviar_msg(msg):
+    """Envia uma mensagem para todos os clientes conectados."""
     for client in clients:
-        client.send(bytes(prefix, "utf8") + msg)
+        client.send(msg)
 
 while True:
     client, client_address = server.accept()
-    enderecos[client] = client_address
     threading.Thread(target=lidar_cliente, args=(client,)).start()
