3346014f10a353d3237d0d353b87973d025377bd
paulo <paulofvilarimda@gmail.com>
2024-08-31 19:36:36 -0300

    docs

--- camada_enlace/correcao/hamming.py
+++ camada_enlace/correcao/hamming.py
@@ -1,6 +1,15 @@
+# Definição das posições dos bits de verificação
 check_bits_positions = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
 
 def Transmissor_hamming_par(msg):
+  """
+  Função que implementa o código de Hamming para a transmissão de uma mensagem.
+
+  Parametros:
+    - msg: string de bits sem o codigo de hamming
+  Retorno:
+    - string de bits com o codigo de hamming incorporado
+  """
   result = []
   i = 0 # index para iterar no result
   j = 0 # index para iterar no msg
@@ -23,6 +32,16 @@ def Transmissor_hamming_par(msg):
   return "".join(result)
 
 def Receptor_hamming_par(msg):
+  """
+  Função que implementa o código de Hamming para a recepção de uma mensagem.
+
+  Parametros:
+    - msg: string de bits com o codigo de hamming incorporado
+  Retorno:
+    - uma tupla no formato (booleano, int)
+      - o booleano é True caso tenha ocorrido erro e False caso não tenha ocorrido erro
+      - o int é o index do bit errado
+  """
   msg_bits = list(msg)
   hamming_bits = ""
   for i in range(0, len(msg_bits)):
@@ -38,11 +57,12 @@ def Receptor_hamming_par(msg):
   for i in range(len(msg_bits) - 1, -1, -1):
     if i + 1 in check_bits_positions:
       hamming_bits += msg_bits[i]
-  return ("1" in hamming_bits, int(hamming_bits, 2))
+  return ("1" in hamming_bits, int(hamming_bits, 2) - 1)
 
+# Exemplo de uso
 msg = "010111"
 msg_hamming_correct = "1000101011"
-msg_hamming_incorrect = "1000001011"  # erro no 5º bit
+msg_hamming_incorrect = "1000001011"  # erro no 5º bit (index 4)
 print(Transmissor_hamming_par(msg))  # esperado: 1000101011
-print(Receptor_hamming_par(msg_hamming_correct))  # esperado: (False, 0)
-print(Receptor_hamming_par(msg_hamming_incorrect))  # esperado: (True, 5)
+print(Receptor_hamming_par(msg_hamming_correct))  # esperado: (False, -1)
+print(Receptor_hamming_par(msg_hamming_incorrect))  # esperado: (True, 4)
